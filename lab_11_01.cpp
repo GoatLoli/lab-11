cc100ed29d94bf470845573762f41985791bcc76
GoatLoli <AsrielDreemurr2804@mail.ru>
2024-03-01 16:43:49 +0200

    .

---
+++ lab_11_01.cpp
@@ -0,0 +1,69 @@
+﻿//(вариант 6) Дана последовательность натуральных чисел{ aj }j = 1...n(n <= 10000).
+// Если в последовательности нет ни одного простого числа, упорядочить последовательность по неубыванию.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+bool isPrime(float* p_mas, int n, int x)
+{
+    for (int i = 0; i < n; i++)
+        int x = p_mas[i];
+
+    if (x < 2)
+        return false;
+    if (x == 2)
+        return true;
+    for (int div = 2; div <= sqrt(x) + 1; div++)
+        if (x % div == 0)
+            return false;
+    return true;
+}
+  
+bool hasPrime(float* p_mas, int n, int x)
+{
+    for (int i = 0; i < n; i++)
+        if (isPrime(p_mas, n, x))
+        {
+            return true;
+            break;
+        }
+    return false; 
+}
+
+
+int main()
+{
+    int n;
+    int x;
+
+    //кол-во
+    std::cout << "kol-vo chisel: ";
+    std::cin >> n;
+
+    //выделение памяти
+
+    float* p_mas = new float[n];
+
+    //ввод
+    std::cout << "chisla v mas: ";
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> p_mas[i];
+    }
+ 
+    //сортировка при отсутствии простых
+    if (hasPrime)
+    {
+        std::sort(p_mas, p_mas + n);
+        std::cout << "prostyh net: ";
+        for (int i = 0; i < n; i++)
+            std::cout << p_mas[i];
+    }
+    else
+        std::cout << "soderzhit prostye";
+
+
+    delete[] p_mas;
+
+}
---
+++ lab_11_02.cpp
@@ -0,0 +1,64 @@
+﻿// (вариант 6) Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m<=100).
+// Найти строку с наибольшим произведением элементов и заменить все элементы этой строки этим произведением
+
+#include <iostream>
+#include <vector>
+
+void maxRowValue(float** p_matrix, int n, int m, int& maxRow, int maxMult)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int mult = 1;
+        for (int j = 0; j < m; j++)
+        {
+            mult *= p_matrix[i][j];
+        }
+        if (mult > maxMult)
+        {
+            maxMult = mult;
+            maxRow = i;
+        }
+    }
+    //замена эл-тов на наибольшее произведение
+    for (int j = 0; j < m; j++)
+    {
+        p_matrix[maxRow][j] = maxMult;
+    }
+}
+
+int main()
+{
+    int n, m;
+    int maxRow = 0;
+    int maxMult = 1;
+
+    std::cout << "razmer matricy: ";
+    std::cin >> n >> m;
+
+    //создание вектора строк для 2ух мерного массива
+    float** p_matrix = new float* [n];
+    for (int i = 0; i < n; i++)
+        p_matrix[i] = new float[m];
+
+    //ввод
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            std::cin >> p_matrix[i][j];
+ 
+    maxRowValue(p_matrix, n, m, maxRow, maxMult);
+
+    //вывод
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            std::cout << p_matrix[i][j] << " ";
+        }
+    }
+
+    //полное удаление
+    for (int i = 0; i < n; i++)
+        delete[] p_matrix[i];
+    delete[] p_matrix;
+}
+
